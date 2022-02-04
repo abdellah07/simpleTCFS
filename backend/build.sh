aea9810c48fed8e1b151466654faf5825e688380
Antoine Heraud <59832812+herauda@users.noreply.github.com>
2022-02-04 17:00:51 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,7 @@
 #Preparing environment
 echo "Compiling the TCF Spring backend"
 mvn clean package
-echo "Done !!"
+echo "Done !!!"
 
 # building the docker image
 docker build --build-arg JAR_FILE=target/simpleTCFS-0.0.1-SNAPSHOT.jar -t pcollet/tcf-spring-backend .
