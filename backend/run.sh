dbdf9a53cef2c0d9b60de85868bb39255b53406c
Joel <joel@dibasso.fr>
2022-02-04 16:53:12 +0100

    useless change in backend

--- backend/run.sh
+++ backend/run.sh
@@ -9,3 +9,4 @@ docker run --rm -d -p 8080:8080 pcollet/tcf-spring-backend
 # to stop: docker stop ID
 # to start a new shell in the container: docker exec -it ID bash
 # to attach to the container: docker attach ID (^P ^Q to detach)
+# bonjour
